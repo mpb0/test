7e9960212a35d250cfb3c0b562f3dccf3d55302b
Manish Barla <37694607+mpb0@users.noreply.github.com>
2020-04-26 14:23:12 +0530

    Revert "Removed comments"
    This reverts commit 6b406bf2e04f5aad05556d959ec0a6537e22a60b.

--- test.py
+++ test.py
@@ -1,3 +1,39 @@
+"""
+#prime numbers
+n=int(input("Enter the value of n"))
+
+x=1
+while n:
+    i=2
+    while i<x:
+        if x%i==0:
+            break
+        else:
+            i+=1
+    if i==x:
+        print(x)
+        n-=1
+    x+=1    
+        
+
+#union of two sets
+n=eval(input("enter the elements of sets"))
+n1=eval(input("enter the elements of set"))
+
+print(n.union(n1))
+
+#common elements in two sets
+n=eval(input("enter the elements of sets"))
+n1=eval(input("enter the elements of set"))
+
+for x in n:
+    for c in n1:
+        if x==c:
+            print(x)
+        else:
+            pass
+
+"""
 
 #cartesian product
 
